8d078f7d550054c50eb4a20e433e752d76f88a95
Remo Inverardi <remo.inverardi@abacus.ch>
2019-08-17 16:55:00 +0200

    Localizing ...

--- src/js/cms-preview-templates/home.js
+++ src/js/cms-preview-templates/home.js
@@ -36,7 +36,7 @@ export default class PostPreview extends React.Component {
             </div>
 
             <div className="tc">
-              <a href="#" className="btn raise">See all products</a>
+              <a href="#" className="btn raise">Alle Produkte anzeigen</a>
             </div>
           </div>
         </div>
@@ -57,7 +57,7 @@ export default class PostPreview extends React.Component {
             </div>
 
             <div className="tc">
-              <a href="{{.buttonLink}}" className="btn raise">Read more</a>
+              <a href="{{.buttonLink}}" className="btn raise">Mehr lesen</a>
             </div>
 
           </div>
