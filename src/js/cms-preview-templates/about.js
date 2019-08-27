661fffc03e82e76c8ea01058807fc63d301c49b1
Remo Inverardi <remo.inverardi@abacus.ch>
2019-08-27 15:51:36 +0200

    Fix the content management.

--- src/js/cms-preview-templates/about.js
+++ src/js/cms-preview-templates/about.js
@@ -3,18 +3,10 @@ import { List } from 'immutable';
 
 import Jumbotron from "./components/jumbotron";
 
-const MediaBlock = ({heading, text, imageUrl, reverse}) => {
-  const imageContainerClassName = reverse
-    ? "ph3-m w-50-m"
-    : "ph3-m w-50-m order-last-m";
-  return <div className="flex-m mhn3-m mb4">
-    <div className={imageContainerClassName}>
-      <img src={imageUrl} alt="" className="db mb2" />
-    </div>
-    <div className="ph3-m w-50-m">
-      <h3 className="f3 b lh-title mb1">{heading}</h3>
-      <p>{text}</p>
-    </div>
+const AboutEntry = ({heading, text}) => {
+  return <div className="center flex-l mhn1-l mw7">
+    <h2 className="b f2 lh-title mb2 w-40-l">{heading}</h3>
+    <p className="mb0 w-60-l">{text}</p>
   </div>;
 };
 
@@ -35,11 +27,9 @@ export default class AboutPreview extends React.Component {
     return <div>
       <Jumbotron image={image} title={entry.getIn(["data", "title"])} />
       <div className="bg-off-white pv4">
-        <div className="mw7 center ph3 pt4">
-          {values.map(({text, heading, imageUrl}, i) =>
-            <MediaBlock key={i} text={text} heading={heading} imageUrl={imageUrl} reverse={i % 2 === 0} />
-          )}
-        </div>
+        {values.map(({text, heading, imageUrl}, i) =>
+          <AboutEntry key={i} text={text} heading={heading} />
+        )}
       </div>
     </div>;
   }
